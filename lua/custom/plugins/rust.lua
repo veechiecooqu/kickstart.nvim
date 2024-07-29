f692a359eab5b9e29a891aa19c74dd5b2cd88f7a
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2024-07-29 15:11:22 +0530

    Remove spellwarn and oil setup

---
+++ lua/custom/plugins/rust.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    config = function()
+      require('crates').setup {
+        lsp = {
+          enabled = true,
+          on_attach = function(client, bufnr)
+            -- the same on_attach function as for your other lsp's
+          end,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      }
+    end,
+  },
+}
